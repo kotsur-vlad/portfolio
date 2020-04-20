b2a230999ad0a89d748d3705fe7bd7955b8d817e
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-20 14:59:15 +0300

    Some refactoring

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
 	return (
 		<div className={styles.footer}>
-			<div className={styles.test}>
-				Footer
-			</div>
+			Footer
 		</div>
 	);
 }
