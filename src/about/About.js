3f98fdd63dc05a59e0f7219c9a0c28a82b66733c
kotsur-vlad <kotsur.vlad@gmail.com>
2020-07-02 12:42:51 +0300

    Added Sass to project, refactored "About" styles

--- src/about/About.js
+++ src/about/About.js
@@ -3,14 +3,13 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faDownload, faCodeBranch, faToolbox} from '@fortawesome/free-solid-svg-icons'
 import {faHtml5, faCss3Alt, faJsSquare, faReact} from '@fortawesome/free-brands-svg-icons'
 
-import styles from './About.module.css'
+import styles from './About.module.scss'
 import Title from "../common/title/Title";
 
 const About = () => {
 	return (
 		<div className={styles.about}>
 			<div className={styles.firstLine}>
-
 				<div className={styles.info}>
 					<Title title="personal info"/>
 					<ul>
@@ -21,7 +20,6 @@ const About = () => {
 						<li>English: intermediate (B1)</li>
 					</ul>
 				</div>
-
 				<div className={styles.photo}>
 					<div className={styles.avatar}/>
 					<span>Front-end Developer</span>
@@ -32,7 +30,6 @@ const About = () => {
 							icon={faCodeBranch}/> GitHub</a></li>
 					</ul>
 				</div>
-
 				<div className={styles.stack}>
 					<Title title="technologies"/>
 					<ul>
@@ -44,26 +41,23 @@ const About = () => {
 					</ul>
 				</div>
 			</div>
-
 			<div className={styles.secondLine}>
-				<div className={styles.soft}>
-					<p>
-						Within my work I’m searching for career and professional growth through
-						deepening knowledge in front-end development. I have strong knowledge of the concept of
-						React-Redux
-						architecture. And I always improve my skills and purposefully learn new technologies. I have
-						some
-						experience in commercial freelance development. In my free time I solve tasks on codewars:&nbsp;
-						<a href="https://www.codewars.com/users/kotsur.vlad/" rel="noopener noreferrer" target="_blank">
-							<img src="https://www.codewars.com/users/kotsur.vlad/badges/micro" alt=""/>
-						</a>
-					</p>
-					<p>
-						I able to work with large amount of information. Also, I can to work hard with tasks, herewith I
-						be
-						persistent. I like good teams with smart people, new challenges, and opportunities.
-					</p>
-				</div>
+				<p>
+					Within my work I’m searching for career and professional growth through
+					deepening knowledge in front-end development. I have strong knowledge of the concept of
+					React-Redux
+					architecture. And I always improve my skills and purposefully learn new technologies. I have
+					some
+					experience in commercial freelance development. In my free time I solve tasks on codewars:&nbsp;
+					<a href="https://www.codewars.com/users/kotsur.vlad/" rel="noopener noreferrer" target="_blank">
+						<img src="https://www.codewars.com/users/kotsur.vlad/badges/micro" alt=""/>
+					</a>
+				</p>
+				<p>
+					I able to work with large amount of information. Also, I can to work hard with tasks, herewith I
+					be
+					persistent. I like good teams with smart people, new challenges, and opportunities.
+				</p>
 			</div>
 		</div>
 	);
