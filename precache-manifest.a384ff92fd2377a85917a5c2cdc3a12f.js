e6d2b36c1557238649d47cf9b4424611b21cfa91
kotsur-vlad <kotsur.vlad@gmail.com>
2020-07-07 11:07:25 +0300

    Updates

---
+++ precache-manifest.a384ff92fd2377a85917a5c2cdc3a12f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bc2832fa5d72225c7a7543b8808dfc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06c2c140af12bb8f42f7",
+    "url": "/portfolio/static/css/main.1ae062fd.chunk.css"
+  },
+  {
+    "revision": "b655d48001b531a3fd2b",
+    "url": "/portfolio/static/js/2.f222e769.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f222e769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c2c140af12bb8f42f7",
+    "url": "/portfolio/static/js/main.57d71e05.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "2eb62b0b8a2278b60647bbd6e970f4b6",
+    "url": "/portfolio/static/media/avatar.2eb62b0b.jpg"
+  },
+  {
+    "revision": "01e51eaebfca712bf731c79ed667b65c",
+    "url": "/portfolio/static/media/bg.01e51eae.png"
+  },
+  {
+    "revision": "a3cf2ec8403e76e8b7e54ae290fc6e90",
+    "url": "/portfolio/static/media/counterPreview.a3cf2ec8.jpg"
+  },
+  {
+    "revision": "ad164d9b3d558c939bb82d89b0e4907e",
+    "url": "/portfolio/static/media/gamePreview.ad164d9b.jpg"
+  }
+]);
